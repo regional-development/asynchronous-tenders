e25776b3fb0ee8eaf390e84676f8ad2305c9e59e
hp0404 <clarencesexual@gmail.com>
2020-09-01 11:41:51 +0300

    Переписав exceptions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 import logging
 import asyncio
@@ -8,20 +9,16 @@ from time import perf_counter
 
 LIMIT = 5
 SLEEP_RANGE = 0.4, 0.8
-SAMPLE_SIZE = 1_000_000
+SAMPLE_SIZE = 100_000
 
 
 async def fetch(sem, session, url):
     filename = url.split("/")[-1]
-    try:
-        async with sem, session.get(url, raise_for_status=True) as response:
-            await asyncio.sleep(random.uniform(*SLEEP_RANGE))
-            with open(f"data/{filename}.json", "wb") as out:
-                async for chunck in response.content.iter_chunked(4096):
-                    out.write(chunck)
-    except aiohttp.client_exceptions.ClientResponseError as exc:
-        logging.info(f"Помилка: {exc} для {filename}")
-        await asyncio.sleep(60)
+    async with sem, session.get(url, raise_for_status=True) as response:
+        await asyncio.sleep(random.uniform(*SLEEP_RANGE))
+        with open(f"data/{filename}.json", "wb") as out:
+            async for chunck in response.content.iter_chunked(4096):
+                out.write(chunck)
 
 
 async def fetch_all(urls, loop):
@@ -31,17 +28,6 @@ async def fetch_all(urls, loop):
             *[fetch(sem, session, url) for url in urls]
         )
         return results
-    
-    
-def main():
-    df = pd.read_csv("./links.csv")
-    sample = df.loc[df["status"].eq(0)].sample(SAMPLE_SIZE)["tender_id"]
-
-    loop = asyncio.get_event_loop()
-    data = loop.run_until_complete(fetch_all(sample, loop))
-
-    df.loc[df["tender_id"].isin(sample), "status"] = 1
-    df.to_csv("./links.csv", index=False)
     
 
 if __name__ == '__main__':
@@ -55,11 +41,35 @@ if __name__ == '__main__':
         ]
     )
 
+    logging.info("Починаю виконувати скрипт")
+
+    already_downloaded = [f.split(".")[0] for f in os.listdir("data")]
+    df = pd.read_csv("./links_ok.csv")
+    sample = df.loc[df["status"].eq(0)].sample(SAMPLE_SIZE)["tender_id"]
+
+    logging.info("Починаю виконувати запити")
     start = perf_counter()
-    main()
+    loop = asyncio.get_event_loop()
+    try:
+        data = loop.run_until_complete(fetch_all(sample, loop))
+        logging.info("Виконав усі запити")
+    except (
+        KeyboardInterrupt,
+        aiohttp.ClientConnectionError,
+        aiohttp.client_exceptions.ClientResponseError
+        ) as e:
+        logging.exception("Сталася помилка")
+        loop.stop()
+        logging.info("Зупинив event loop")
     stop = perf_counter()
 
-    time = stop - start 
+    df["tenders"] = df["tender_id"].str.split("/").str[-1]
+    downloaded = [f.split(".")[0] for f in os.listdir("data")]
+    df.loc[df["tenders"].isin(downloaded), "status"] = 1
+    df.drop("tenders", 1).to_csv("./links_ok.csv")
+
+    time, N = stop - start, len(downloaded) - len(already_downloaded) 
     logging.info(
-        f"N: {SAMPLE_SIZE}, total time: {time:.2f}; {SAMPLE_SIZE/time:.2f}/1сек" 
+        f"Завантажив {N} тендерів за {time:.2f}s; {N/time:.2f}/1сек" 
     )
+    logging.info("Закінчив виконувати скрипт \n")
