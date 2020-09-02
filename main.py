77d72703d958efc731dbd171eb6c21528d06018d
hp0404 <clarencesexual@gmail.com>
2020-09-02 13:21:34 +0300

    try except для fetch на прикладі тендерів; #6

--- main.py
+++ main.py
@@ -7,45 +7,40 @@ from pathlib import Path
 from time import perf_counter 
 
 
-URLS = ["https://httpbin.org/status/502"] * 6 + ["https://httpbin.org/status/429"] * 2 + ["https://httpbin.org/status/200"] * 50
 PATH = Path(__file__).resolve().parent
 DATA = PATH / "data"
 
-LIMIT = 5
 TIMEOUT = 60
 SLEEP_RANGE = 0.4, 0.8
-SAMPLE_SIZE = 150_000
+SAMPLE_SIZE = 1000
+CONCURRENT_CONNECTIONS = 5
 
 
 async def fetch(sem, session, url):
-    counter = 3
-    fetched = False
     filename = url.split("/")[-1]
-    # logging.info(f"Початок fetch для url: {url}; counter: {counter}, fetched: {fetched}")
+    fetched, counter = False, 3
     while not fetched:
         async with sem:
             try:
                 await asyncio.sleep(random.uniform(*SLEEP_RANGE))
                 async with session.get(url, raise_for_status=True) as response:
-                    # logging.info(f"Виконав запит для {url}")
-                    # await asyncio.sleep(random.uniform(*SLEEP_RANGE))
                     with open(DATA / f"{filename}.json", "wb") as out:
                         async for chunk in response.content.iter_chunked(4096):
                             out.write(chunk)
-                    logging.info(f"Успішно записав файл для {url}")
                     fetched = True
-            except aiohttp.client_exceptions.ClientResponseError:
-                logging.info(f"Не зумів викнати запит для {url}, лягаю спати: counter {counter}")
+            except (
+                aiohttp.client_exceptions.ClientOSError,
+                aiohttp.client_exceptions.ClientResponseError
+            ):
                 await asyncio.sleep(TIMEOUT)
                 counter -= 1
                 if counter < 1:
-                    logging.info(f"Не записав файл для {url}, counter: {counter}, fetched: {fetched}")
+                    logging.info(f"Помилка: {filename}")
                     break 
-    # logging.info(f"Завершив fetch для {url}, counter: {counter}, fetched: {fetched}")
 
 
 async def fetch_all(urls, loop):
-    sem = asyncio.BoundedSemaphore(LIMIT)
+    sem = asyncio.BoundedSemaphore(CONCURRENT_CONNECTIONS)
     async with aiohttp.ClientSession(loop=loop) as session:
         results = await asyncio.gather(
             *[fetch(sem, session, url) for url in urls]
@@ -55,8 +50,6 @@ async def fetch_all(urls, loop):
 
 if __name__ == '__main__':
 
-    random.shuffle(URLS)
-    
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s]: %(message)s",
@@ -68,15 +61,15 @@ if __name__ == '__main__':
 
     logging.info("Починаю виконувати скрипт")
 
-    # already_downloaded = [f.parts[-1].split(".")[0] for f in DATA.rglob("*.json")]
-    # df = pd.read_csv(PATH / "links.csv")
-    # sample = df.loc[df["status"].eq(0)].sample(SAMPLE_SIZE)["tender_id"]
+    already_downloaded = [f.parts[-1].split(".")[0] for f in DATA.rglob("*.json")]
+    df = pd.read_csv(PATH / "links.csv")
+    sample = df.loc[df["status"].eq(0)].sample(SAMPLE_SIZE)["tender_id"]
 
     logging.info("Починаю виконувати запити")
     start = perf_counter()
     loop = asyncio.get_event_loop()
     try:
-        data = loop.run_until_complete(fetch_all(URLS, loop))
+        data = loop.run_until_complete(fetch_all(sample, loop))
         logging.info("Виконав усі запити")
     except:
         logging.exception("Сталася помилка")
@@ -84,13 +77,13 @@ if __name__ == '__main__':
         logging.info("Зупинив event loop")
     stop = perf_counter()
 
-    # df["tenders"] = df["tender_id"].str.split("/").str[-1]
-    # downloaded = [f.parts[-1].split(".")[0] for f in DATA.rglob("*.json")]
-    # df.loc[df["tenders"].isin(downloaded), "status"] = 1
-    # df.drop("tenders", 1).to_csv(PATH / "links.csv")
+    df["tenders"] = df["tender_id"].str.split("/").str[-1]
+    downloaded = [f.parts[-1].split(".")[0] for f in DATA.rglob("*.json")]
+    df.loc[df["tenders"].isin(downloaded), "status"] = 1
+    df.drop("tenders", 1).to_csv(PATH / "links.csv")
 
-    # time, N = stop - start, len(downloaded) - len(already_downloaded) 
-    # logging.info(
-    #     f"Завантажив {N} тендерів за {time:.2f}s; {N/time:.2f}/1сек" 
-    # )
-    # logging.info("Закінчив виконувати скрипт \n")
+    time, N = stop - start, len(downloaded) - len(already_downloaded) 
+    logging.info(
+        f"Завантажив {N} тендерів за {time:.2f}s; {N/time:.2f}/1сек" 
+    )
+    logging.info("Закінчив виконувати скрипт \n")
