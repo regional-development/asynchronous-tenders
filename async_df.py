b458122c1d324af755bf1891eec00917060b7a38
hp0404 <hryhorii.pavlenko@gmail.com>
2020-09-10 13:18:39 +0300

    Зчитування переліку тендерів з та збереження в .csv

--- async_df.py
+++ async_df.py
@@ -2,8 +2,12 @@ import random
 import asyncio
 import aiohttp
 import pandas as pd
+from pathlib import Path
 
 
+PATH = Path(__file__).resolve().parent
+INPUTS = PATH / "sample.csv"
+OUTPUTS = PATH / "output_table.csv"
 SLEEP_RANGE = 0.6, 0.7
 CONCURRENT_CONNECTIONS = 5
 
@@ -12,7 +16,10 @@ async def fetch(sem, session, url):
     async with sem, session.get(url) as response:
         await asyncio.sleep(random.uniform(*SLEEP_RANGE))
         data = await response.json()
-        return {"tenderid": url.split("/")[-1], "data": data}
+        return {
+            "ProzorroTenderID": url.split("/")[-1], 
+            "JSON_Value": data.get("data", "error")
+        } 
 
 
 async def fetch_all(urls, loop):
@@ -21,21 +28,24 @@ async def fetch_all(urls, loop):
         results = await asyncio.gather(
             *[fetch(sem, session, url) for url in urls]
         )
-        return pd.DataFrame(results)
+        df = pd.DataFrame(results)
+        return df.loc[df["JSON_Value"].ne("error")]
+
+
+def read_tenders():
+    df = pd.read_csv(INPUTS, sep='\t', header=None)
+    df.columns = ["ProzorroTenderID"]
+    df["ProzorroTenderID"] = (
+        "https://public.api.openprocurement.org/api/2.5/tenders/" + 
+        df["ProzorroTenderID"]
+    )
+    return df["ProzorroTenderID"].tolist()
 
 
 if __name__ == '__main__':
-
-    urls = (
-        "https://public.api.openprocurement.org/api/2.5/tenders/6a0585fcfb05471796bb2b6a1d379f9b",
-        "https://public.api.openprocurement.org/api/2.5/tenders/d1c74ec8bb9143d5b49e7ef32202f51c",
-        "https://public.api.openprocurement.org/api/2.5/tenders/a3ec49c5b3e847fca2a1c215a2b69f8d",
-        "https://public.api.openprocurement.org/api/2.5/tenders/52d8a15c55dd4f2ca9232f40c89bfa82",
-        "https://public.api.openprocurement.org/api/2.5/tenders/b3af1cc6554440acbfe1d29103fe0c6a",
-        "https://public.api.openprocurement.org/api/2.5/tenders/1d1c6560baac4a968f2c82c004a35c90",
-        "https://public.api.openprocurement.org/api/2.5/tenders/1d1c6560qwerty",
-    ) 
-
+    
+    urls = read_tenders()
     loop = asyncio.get_event_loop()
     data = loop.run_until_complete(fetch_all(urls, loop))
     print(data)
+    data.to_csv(OUTPUTS, index=False)
