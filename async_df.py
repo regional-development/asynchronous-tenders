9886814fd1dca347b21eed4b9f668bde812b80f4
hp0404 <hryhorii.pavlenko@gmail.com>
2020-09-10 12:12:11 +0300

    raise_for_status=False і датафрейм не ламається в #10

--- async_df.py
+++ async_df.py
@@ -9,7 +9,7 @@ CONCURRENT_CONNECTIONS = 5
 
 
 async def fetch(sem, session, url):
-    async with sem, session.get(url, raise_for_status=True) as response:
+    async with sem, session.get(url) as response:
         await asyncio.sleep(random.uniform(*SLEEP_RANGE))
         data = await response.json()
         return {"tenderid": url.split("/")[-1], "data": data}
@@ -33,6 +33,7 @@ if __name__ == '__main__':
         "https://public.api.openprocurement.org/api/2.5/tenders/52d8a15c55dd4f2ca9232f40c89bfa82",
         "https://public.api.openprocurement.org/api/2.5/tenders/b3af1cc6554440acbfe1d29103fe0c6a",
         "https://public.api.openprocurement.org/api/2.5/tenders/1d1c6560baac4a968f2c82c004a35c90",
+        "https://public.api.openprocurement.org/api/2.5/tenders/1d1c6560qwerty",
     ) 
 
     loop = asyncio.get_event_loop()
